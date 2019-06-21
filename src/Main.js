f505930e37891980cb453baf698207995cf4d750
Tristan Gruener <tristangruener@Tristans-iMac.attlocal.net>
2019-06-20 22:37:16 -0700

    init

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            recipe collective
+          </h1>
+          <p className="subtitle">
+            hey
+          </p>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Main
