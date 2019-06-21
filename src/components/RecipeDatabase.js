726caf03dab1123114234c979bbb01007fa090bf
Tristan Gruener <tristangruener@Tristans-iMac.attlocal.net>
2019-06-21 07:43:55 -0700

    recipe db layout work

--- src/components/RecipeDatabase.js
+++ src/components/RecipeDatabase.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 class RecipeDatabase extends React.Component {
+
+  //can click into recipe.... how we gonna do that?
+  //i want recipe to be clicked upon, and when you click it it will render into the main area... the recipe list can be on the sidebar
+  //so it shall be two columns
+
   constructor() {
     super()
     this.state = {
@@ -27,12 +32,25 @@ class RecipeDatabase extends React.Component {
               <div className="page-title">
                 <h3 class="has-text-grey-light">recipe database</h3>
               </div>
-              <div className="page-content">
-                {this.state.recipes.map((recipe) => (
-                  <div class="recipe-link">
-                    <p class="has-text-grey">{recipe.name}</p>
+            </div>
+            <div className="container">
+              <div className="columns">
+                <div className="column is-two-fifths">
+                  <div className="content">
+                    <div className="page-content">
+                      {this.state.recipes.map((recipe) => (
+                        <div class="recipe-link">
+                          <p class="has-text-grey">{recipe.name}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                ))}
+                </div>
+                <div className="column">
+                  <div className="content">
+                    <i class="has-text-grey-light">recipe content</i>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
