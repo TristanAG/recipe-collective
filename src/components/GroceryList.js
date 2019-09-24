35adec7614f9d50ccdd74c2bfa4f78961ba5b626
Tristan Gruener <tristan.gruener@gmail.com>
2019-09-23 21:49:10 -0700

    converted 2 files

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -1,17 +1,15 @@
 import React from 'react'
 
-class GroceryList extends React.Component {
-  render() {
-    return (
-      <section className="grocery-list">
-        <div className="container">
-          <div className="content">
-            <h3 className="has-text-grey-light">grocery list</h3>
-          </div>
+function GroceryList() {
+  return (
+    <section className="grocery-list">
+      <div className="container">
+        <div className="content">
+          <h3 className="has-text-grey-light">grocery list</h3>
         </div>
-      </section>
-    )
-  }
+      </div>
+    </section>
+  )
 }
 
 export default GroceryList
--- src/components/WeekPlanner.js
+++ src/components/WeekPlanner.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
-class WeekPlanner extends React.Component {
-  render() {
-    return (
-      <div>
-        <section className="Home">
-          <div className="container">
-            <div className="content">
-              <h3 className="has-text-grey-light">week planner</h3>
-            </div>
-          </div>
-        </section>
+function WeekPlanner() {
+  return (
+    <section className="Home">
+      <div className="container">
+        <div className="content">
+          <h3 className="has-text-grey-light">week planner</h3>
+        </div>
       </div>
-    )
-  }
+    </section>
+  )
 }
 
 export default WeekPlanner
