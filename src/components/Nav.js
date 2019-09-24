e1265ba2fa928809637102e2c252a6687e55806e
Tristan Gruener <tristan.gruener@gmail.com>
2019-09-24 00:46:13 -0700

    style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,10 +7,10 @@ class Nav extends React.Component {
       <div className="columns">
         <div className="column is-one-third logo">
           <div className="content">
-            <Link to="/"><h3 className="has-text-grey">rcpclctv</h3></Link>
+            <Link to="/"><h3 className="has-text-grey">🌱&nbsp; r e c i p e &nbsp; c o l l e c t i v e </h3></Link>
           </div>
         </div>
-        {/* <div className="column">
+        <div className="column">
           <div className="content">
             <Link to="/recipe-database/" className="nav-link"><h5 className="has-text-grey-dark">recipe db</h5></Link>
           </div>
@@ -29,9 +29,9 @@ class Nav extends React.Component {
           <div className="content">
             <Link to="/grocery-list/" className="nav-link"><h5 className="has-text-grey-dark">grocery list</h5></Link>
           </div>
-        </div> */}
+        </div>
         <div className="column">
-          <div className="content">
+          <div className="content has-text-right">
             <Link to="/login/" className="nav-link"><h5 className="has-text-success">login</h5></Link>
           </div>
         </div>
